7c6377732902d162cd93c0cdfd0e194ea36b8dce
com.Kondratov <com.Kondratov@gmail.com>
2020-10-07 07:13:21 +0300

    Добавьте файлы проекта.

---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [Route("api/emails")]
+    public class EmailController : ControllerBase
+    {
+        public void Post()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
